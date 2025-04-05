3d846a181b6fb70d69fba99456fa8b402a7739e1
Zhan Setya <ardy67508@gmail.com>
2025-04-06 03:26:56 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -4,9 +4,7 @@ import asyncio
 client = NewClient("Zzzz")
 
 async def spamming(number):
-    for i in range(1, 5):
-        await client.PairPhone(number, show_push_notification=True)
-        print(f"Pairing percobaan ke-{i}")
+    await client.PairPhone(number, show_push_notification=True)
 
 async def main():
     nomor = input("Masukkan nomor WhatsApp (cth: 628xxxx): ")
